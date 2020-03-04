09e0c153532e8b88c2beb62fb5163f420aff7a4f
Vincent Akena <vakena@outlook.com>
2020-03-04 15:16:10 +1100

    code implemented to get website

--- automation.py
+++ automation.py
@@ -1,1 +1,8 @@
-print("Assessment Automation")
+from bs4 import BeautifulSoup
+import requests
+
+# Get website
+website = requests.get('https://www.studiobinder.com/blog/best-filmmaking-software-and-tools/#Story-Development')
+soup = BeautifulSoup(website.content, 'html.parser')
+content = soup.find('div', class_='thrv_wrapper thrv_text_element')
+print(content)
