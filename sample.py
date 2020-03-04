2d06c43d1cf15c22fa9fc218aecc805011b055a5
Vincent Akena <vakena@outlook.com>
2020-03-04 19:03:36 +1100

    save output to file functionality

--- sample.py
+++ sample.py
@@ -1,13 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Load Networking and Cybersecurity Major
-source = requests.get('https://handbook.uts.edu.au/directory/maj03445.html').text
+# Load website from URL
+source = requests.get(
+    'https://handbook.uts.edu.au/directory/maj03445.html').text
+
 # Create soup object and load parser
 soup = BeautifulSoup(source, 'lxml')
+# Create file using extension of choice
+file = open('subjects.pdf', 'w+')
 
 # Select all subjects in the 'Completion Requirements' table
 subject_names = soup.find_all('table')
 # For every subject in the table, display the text attribute
 for subject in subject_names:
-    print(subject.text)
+    # Writes output to file
+    print(subject.text, file=file)
+
+file.close()
