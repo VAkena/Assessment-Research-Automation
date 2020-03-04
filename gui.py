61e83b39b182760fe6ce07936a443242bdda8025
Vincent Akena <vakena@outlook.com>
2020-03-04 20:47:05 +1100

    gui file created

---
+++ gui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+# Defining the root window
+root = Tk()
+root.geometry("600x400")
+
+label = ttk.Label(root, text="Assessment Automation")
+label.pack()
